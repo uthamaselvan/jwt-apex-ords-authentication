b4e74af705c299088be3e7a34e111b17d4b9ea8c
uthamaselvan <62805136+uthamaselvan@users.noreply.github.com>
2026-03-06 01:30:11 +0530

    Add files via upload

---
+++ sample_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_master (user_name, user_role) VALUES ('AdminUser', 'ADMIN');
+INSERT INTO user_master (user_name, user_role) VALUES ('Uthaman', 'ADMIN');
+INSERT INTO user_master (user_name, user_role) VALUES ('Ravi', 'OPERATOR');
+INSERT INTO user_master (user_name, user_role) VALUES ('Karthik', 'VIEWER');
